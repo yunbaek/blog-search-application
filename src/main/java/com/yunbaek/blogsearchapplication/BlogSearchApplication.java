8faad611dec74c82d8a503fffd783b9cd7dc87d4
yunbaek <yunbaek4564@gmail.com>
2023-03-19 17:52:06 +0900

    Initial commit

---
+++ src/main/java/com/yunbaek/blogsearchapplication/BlogSearchApplication.java
@@ -0,0 +1,13 @@
+package com.yunbaek.blogsearchapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSearchApplication.class, args);
+	}
+
+}
