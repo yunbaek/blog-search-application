4991e9b14f215a091079ae6e379c0454c6186990
yunbaek <yunbaek4564@gmail.com>
2023-03-22 02:32:47 +0900

    feat: 블로그 검색 API 추가

--- src/main/java/com/yunbaek/blogsearchapplication/client/dto/BlogSearchRequest.java
+++ src/main/java/com/yunbaek/blogsearchapplication/client/dto/BlogSearchRequest.java
@@ -1,5 +1,7 @@
 package com.yunbaek.blogsearchapplication.client.dto;
 
+import java.util.Objects;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
@@ -24,6 +26,33 @@ public class BlogSearchRequest {
 		this.sort = sort == null ? Sort.ACCURACY : sort;
 		this.page = page == null ? 1 : page;
 		this.size = size == null ? 10 : size;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		BlogSearchRequest that = (BlogSearchRequest)o;
+
+		if (!Objects.equals(query, that.query))
+			return false;
+		if (sort != that.sort)
+			return false;
+		if (!Objects.equals(page, that.page))
+			return false;
+		return Objects.equals(size, that.size);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = query != null ? query.hashCode() : 0;
+		result = 31 * result + (sort != null ? sort.hashCode() : 0);
+		result = 31 * result + (page != null ? page.hashCode() : 0);
+		result = 31 * result + (size != null ? size.hashCode() : 0);
+		return result;
 	}
 }
 
---
+++ src/main/java/com/yunbaek/blogsearchapplication/ui/BlogSearchController.java
@@ -0,0 +1,30 @@
+package com.yunbaek.blogsearchapplication.ui;
+
+import javax.validation.Valid;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yunbaek.blogsearchapplication.application.BlogSearchService;
+import com.yunbaek.blogsearchapplication.client.dto.BlogSearchRequest;
+import com.yunbaek.blogsearchapplication.ui.dto.BlogSearchResponse;
+
+@RestController
+public class BlogSearchController {
+
+	private final BlogSearchService blogSearchService;
+
+	public BlogSearchController(BlogSearchService blogSearchService) {
+		this.blogSearchService = blogSearchService;
+	}
+
+	@Cacheable(value = "blogList", key = "#request", unless = "#result == null")
+	@GetMapping("/blogs")
+	@ResponseStatus(HttpStatus.OK)
+	public BlogSearchResponse getResults(@Valid BlogSearchRequest request) {
+		return blogSearchService.search(request);
+	}
+}
